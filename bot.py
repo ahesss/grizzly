0362122d56fc93802d4e377fd08c253ba73ff441
ahesss <mangkok.pepes01@gmail.com>
2026-03-09 03:19:50 +0700

    Add aggressive /autobuy feature for Vietnam

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-# Trigger redeploy - Persistence Test 2
+# Trigger redeploy - Persistence Test Final Check
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
 import sqlite3
